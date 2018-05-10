789356024bd88d8e1fe06f8e2b0356d937e3a606
Sampath Tharanga <sampaththaranga13@gmail.com>
2018-05-10 11:53:02 +0530

    Working Correctly this application. Also added some screenshots.

---
+++ MVC_Data_Table/MVC_Data_Table/Controllers/HomeController.cs
@@ -0,0 +1,28 @@
+﻿using MVC_Data_Table.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVC_Data_Table.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        //Write action for return database data
+        public ActionResult loaddata()
+        {
+            using (MyDatabaseEntities dc = new MyDatabaseEntities())
+            {
+                var data = dc.Cusomers.OrderBy(a => a.CompanyName).ToList();
+                return Json(new { data = data }, JsonRequestBehavior.AllowGet);
+            }
+        }
+    }
+}
